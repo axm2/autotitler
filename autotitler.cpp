5750c7b982b7e82b4101d4d26652467d4b6a6994
axm2 <axmirza2@yahoo.com>
2020-08-26 01:44:41 -0400

    remove test comments

--- autotitler.cpp
+++ autotitler.cpp
@@ -7,7 +7,6 @@
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 
-// for convenience
 using json = nlohmann::json;
 
 std::string exec(const char* cmd) {
@@ -32,18 +31,15 @@ int main (){
 	
     std::string app_api_key = "";
     if (const char* env_p = std::getenv("ACOUSTID_KEY"))
-        //std::cout << env_p << std::endl;
         app_api_key = std::string (env_p);
     else{
         std::cout << "NO API KEY FOUND" << std::endl;
         return 1;
     }
-        //app_api_key = "ZKnVrQZ0du";
     char* cmd = "fpcalc fjj.flac -json";
 	std::string fpcalc_output = exec(cmd);
 	
     auto j = json::parse(fpcalc_output);
-    //std::cout << j["fingerprint"] << std::endl;
     std::string url = "https://api.acoustid.org/v2/lookup";
     std::string clienturlss = "?client=" + app_api_key;
     std::string durationss = "&duration=" + std::to_string(j["duration"].get<int>());
@@ -58,7 +54,6 @@ int main (){
 
   	curl = curl_easy_init();
   	if(curl) {
-        // We have to combine, then convert to cstr then pass to CURL
         curl_easy_setopt(curl,CURLOPT_URL,final_url.c_str());
     	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
@@ -67,7 +62,6 @@ int main (){
 
         auto j_res = json::parse(readBuffer);
         std::cout << j_res["results"][0]["recordings"][0]["title"] << std::endl;
-    	//std::cout << readBuffer << std::endl;
   	}
 	return 0;
 }
